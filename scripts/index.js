8fc3bd841bf3813740360c329e73489a1ca5380e
John Nguyen <nguyen.johnsan@gmail.com>
2019-05-06 14:05:18 -0700

    first commit

---
+++ scripts/index.js
@@ -0,0 +1,118 @@
+const startBtn = document.getElementById("startBtn");
+const boxes = [...document.getElementsByClassName("box")];
+const gameDescription = document.getElementById("gameDescription");
+
+const possibleAnswers = {
+  "green": 0,
+  "red": 1,
+  "yellow": 2,
+  "blue": 3
+}
+
+let level = 1;
+let sequence = 0;
+let answers = [];
+let playing = false;
+
+
+function toggleActive(num) {
+  boxes[num].classList.toggle("active");
+}
+
+function gameToggle(num) {
+  toggleActive(num);
+  setTimeout(function() {
+    toggleActive(num);
+  }, 500);
+}
+
+function generateNum() {
+  const randomNum = Math.random().toFixed(2);
+
+  if (randomNum >= 0 && randomNum < 0.25) {
+    return 0;
+  } else if (randomNum >= 0.25 && randomNum < 0.5) {
+    return 1;
+  } else if (randomNum >= 0.5 && randomNum < 0.75) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+function updateLevelText() {
+  const levelText = document.getElementById("level");
+  levelText.innerText = `Level ${level}`;
+}
+
+function updateGameDescription(message) {
+  gameDescription.innerText = message;
+}
+
+function clearGameDescription() {
+  gameDescription.innerText = "";
+}
+
+function startGame() {
+  playing = true;
+  updateGameDescription("game started");
+  level = 1;
+  startBtn.setAttribute("disabled", true);
+  playRound();
+}
+
+function playRound() {
+  updateLevelText();
+  updateGameDescription("Watch");
+  let newNum = generateNum();
+  answers.push(newNum);
+  console.log(`answers: ${answers}`);
+
+  for (let i = 0; i < answers.length; i++) {
+    setTimeout(function() {
+      gameToggle(answers[i]);
+    }, i * 1000);
+  }
+
+  updateGameDescription("Now click");
+}
+
+function checkAnswer(event) {
+  const userAnswer = possibleAnswers[event.target.id];
+  const correctAnswer = answers[sequence];
+
+  if (userAnswer == correctAnswer) {
+    updateGameDescription("good job! :)");
+    sequence++;
+  } else {
+    updateGameDescription("You lose. :(");
+    sequence = 0;
+    answers = [];
+    startBtn.removeAttribute("disabled");
+    playing = false;
+  }
+
+  // when you reached the last correct answer, play new round
+
+  if (sequence == answers.length && playing == true) {
+    updateGameDescription("nice job! round end, starting new round...");
+    setTimeout(() => {
+      sequence = 0;
+      level++;
+      playRound();
+    }, 1250);
+  }
+}
+
+startBtn.addEventListener("click", startGame);
+
+
+boxes.forEach((box) => {
+  box.addEventListener("click", function(e) {
+    if (playing == true) {
+      let color = e.target.id;
+      gameToggle(possibleAnswers[color]);
+      checkAnswer(e);
+    }
+  });
+});
